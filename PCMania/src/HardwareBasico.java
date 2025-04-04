d6b4cb82102fe63986bb82df4d3d7a3a8ae5fe08
vitordias2004 <vitordias2004.n@gmail.com>
2025-04-04 12:32:53 -0300

    Add files via upload

---
+++ PCMania/src/HardwareBasico.java
@@ -0,0 +1,9 @@
+public class HardwareBasico {
+    String nome;
+    float capacidade;
+
+    public HardwareBasico(String nome, float capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+}
---
+++ PCMania/src/MemoriaUSB.java
@@ -0,0 +1,9 @@
+public class MemoriaUSB {
+    String nome;
+    int capacidade;
+
+    public MemoriaUSB(String nome, int capacidade) {
+        this.nome = nome;
+        this.capacidade = capacidade;
+    }
+}
