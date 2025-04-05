c11e2d0fe17420e8482beecab91a1bc6568fb77d
vitordias2004 <vitordias2004.n@gmail.com>
2025-04-04 23:15:57 -0300

    Add files via upload

---
+++ PCMania/src/SistemaOperacional.java
@@ -0,0 +1,9 @@
+public class SistemaOperacional {
+    String nome;
+    int tipo;
+
+    public SistemaOperacional(String nome, int tipo) {
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+}
